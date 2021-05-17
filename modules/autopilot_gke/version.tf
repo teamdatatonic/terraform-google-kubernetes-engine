03cad235b296870602c0094b7db6aa725610450b
simonbutt-datatonic <simon.butt@datatonic.com>
2021-05-17 23:16:16 +0100

    feat(autopilot): added an autopilot module
    Signed-off-by: simonbutt-datatonic <simon.butt@datatonic.com>

---
+++ modules/autopilot_gke/version.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.14.5"
+
+  required_providers {
+    google = {
+      version = "~> 3.55"
+    }
+
+    google-beta = {
+      version = "~> 3.55"
+    }
+  }
+
+}
